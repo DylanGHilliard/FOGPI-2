271db816d13174c8af4feb255f2770bdeaf62e54
Dylan Hilliard <ghilliard69420@gmail.com>
2025-08-28 18:17:10 -0500

    Added basic player movment and started weapon logic

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera sceneCamera;
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    private Vector2 mousePos;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>() == null ? gameObject.AddComponent<Rigidbody2D>() : GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+        rb.freezeRotation = true;
+        
+    }
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement = movement.normalized;
+
+        mousePos = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+
+        // Rotate player to follow mouse
+        Vector2 aimDirection = mousePos - rb.position;
+        float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
