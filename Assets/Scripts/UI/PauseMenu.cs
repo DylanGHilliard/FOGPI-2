4462345fd5bf1b8ae82c9460091ed7859e739b73
Dylan Hilliard <ghilliard69420@gmail.com>
2025-09-02 17:41:24 -0500

    Basic Pause Menu and TMPro import

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePannel;
+    public bool isPaused;
+
+    void Start()
+    {
+        pausePannel.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+
+            if (isPaused)
+            {
+
+                Time.timeScale = 0;
+                pausePannel.SetActive(true);
+                isPaused = true;
+            }
+            else Resume();
+
+        }
+    }
+
+
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        pausePannel.SetActive(false);
+        isPaused = false;
+    }
+}
